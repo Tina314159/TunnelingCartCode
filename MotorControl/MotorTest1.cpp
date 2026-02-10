a73af358c7ed4e810461237188e02e1db79e450e
Tina314159 <127436727+Tina314159@users.noreply.github.com>
2026-02-10 02:10:54 +0000

    modified LA file

--- MotorControl/MotorTest1.cpp
+++ MotorControl/MotorTest1.cpp
@@ -6,12 +6,12 @@ int main() {
     gpiod_chip* chip = gpiod_chip_open_by_name("gpiochip0");
 
     // Get GPIO lines
-    gpiod_line* pin17 = gpiod_chip_get_line(chip, 17);
-    gpiod_line* pin18 = gpiod_chip_get_line(chip, 18);
+    gpiod_line* LApin1 = gpiod_chip_get_line(chip, 17);  //LA = linear actuator pin
+    gpiod_line* LApin2 = gpiod_chip_get_line(chip, 18);
 
     // Set pins as outputs, start LOW
-    gpiod_line_request_output(pin17, "actuator", 0);
-    gpiod_line_request_output(pin18, "actuator", 0);
+    gpiod_line_request_output(LApin1, "linear_actuator", 0);
+    gpiod_line_request_output(LApin2, "linear_actuator", 0);
 
     int input;
 
@@ -22,18 +22,18 @@ int main() {
 
         if (input == 1) {
             // Extend
-            gpiod_line_set_value(pin17, 1);
-            gpiod_line_set_value(pin18, 0);
+            gpiod_line_set_value(LApin1, 1);
+            gpiod_line_set_value(LApin2, 1);
         }
         else if (input == 2) {
             // Retract
-            gpiod_line_set_value(pin17, 0);
-            gpiod_line_set_value(pin18, 1);
+            gpiod_line_set_value(LApin1, 0);
+            gpiod_line_set_value(LApin2, 0);
         }
         else if (input == 0) {
             // Stop
-            gpiod_line_set_value(pin17, 0);
-            gpiod_line_set_value(pin18, 0);
+            gpiod_line_set_value(LApin1, 0);
+            gpiod_line_set_value(LApin2, 0);
         }
     }
 }
