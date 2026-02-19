222473cb9d221b1785c2d1cf34548f3244114e6c
yagoavello4 <yagoavello4@gmail.com>
2026-02-18 17:52:56 -0800

    Update linear_actuator.cpp

--- GPIO_exp/linear_actuator.cpp
+++ GPIO_exp/linear_actuator.cpp
@@ -8,38 +8,48 @@ int main() {
     gpiod_chip* chip = gpiod_chip_open_by_name("gpiochip4");
 
     // Get GPIO lines
-    gpiod_line* pin17 = gpiod_chip_get_line(chip, 22);
+    gpiod_line* pin22 = gpiod_chip_get_line(chip, 22);
     gpiod_line* pin18 = gpiod_chip_get_line(chip, 23);
+    gpiod_line* powerCut = gpiod_chip_get_line(chip, 24);
 
     // Set pinl............................................................................................,l;;;;;;;;;;;;;;;;;;;;;;;;;;;,ll s as outputs, start LOW
     gpiod_line_request_output(pin17, "actuator", 0);
     gpiod_line_request_output(pin18, "actuator", 0);
+    gpiod_line_request_output(powerCut, "actuator", 0);
 
     int input;
 
-    std::cout << "Enter 1 to extend, 2 to retract, 0 to stop:\n";
+    std::cout << "0= stop, 1=power on, 2=extend, 3=retract, 4=exit\n";
 
     while (true) {
         std::cin >> input;
-
-        if (input == 1) {
+        if (input == 0) {
+            std::cout << "Power Off \n" ;
+            gpiod_line_set_value(powerCut, 0);
+        }
+        else if(input == 1){
+            std::cout << "Power On \n" ;
+            gpiod_line_set_value(powerCut, 1);            
+            
+        else if (input == 2) {
             // Extend
             std::cout << "Mode 1 \n" ;
             gpiod_line_set_value(pin17, 1);
             gpiod_line_set_value(pin18, 1);
         }
-        else if (input == 0) {
+        else if (input == 3) {
             // Retract
-              std::cout << "Mode 0 \n" ;
+              std::cout << "Mode 1 \n" ;
             gpiod_line_set_value(pin17, 0);
             gpiod_line_set_value(pin18, 0);
         }
         
-        else if(input == 2 ){
+        else if(input == 4 ){
           
-              gpiod_line_release(pin17);
-                gpiod_line_release(pin18);
-                        gpiod_chip_close(chip);
+            gpiod_line_release(pin17);
+            gpiod_line_release(pin18);
+            gpiod_chip_close(chip);
+            gpiod_line_release(powerCut);
         
        } 
     }
